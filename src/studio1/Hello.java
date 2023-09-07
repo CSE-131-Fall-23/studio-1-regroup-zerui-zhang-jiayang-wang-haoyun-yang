f624fc3be3745ba90dfd1342fd717f4a7f70ddf7
wangjiayang <wangjiayang@localhost>
2023-09-07 12:37:18 -0500

    we are done

--- src/studio1/Hello.java
+++ src/studio1/Hello.java
@@ -5,10 +5,10 @@ import java.util.Scanner;
 public class Hello {
 	
 	public static void main(String[] args) {
-		Scanner in = new Scanner(System.in);
+		Scanner scan = new Scanner(System.in);
 		
 		System.out.println("How are you feeling today?");
-		String mood = in.nextLine();
+		String mood = scan.nextLine();
 		
 		System.out.println("Hello and welcome to CSE 131!");
 		System.out.println("You seem to be feeling " + mood);
--- src/studio1/HiFour.java
+++ src/studio1/HiFour.java
@@ -8,15 +8,18 @@ import java.util.Scanner;
  */
 public class HiFour {
 	public static void main(String[] args) {
-		Scanner in = new Scanner(System.in);
-		System.out.print("Enter value for arg 0: ");
-		String s0 = in.nextLine();
-		System.out.print("Enter value for arg 1: ");
-		String s1 = in.nextLine();
-		System.out.print("Enter value for arg 2: ");
-		String s2 = in.nextLine();
-		System.out.print("Enter value for arg 3: ");
-		String s3 = in.nextLine();
+		Scanner scan = new Scanner(System.in);
+		System.out.print("Enter youer name: ");
+		String s0= scan.nextLine();
+		System.out.print("Enter youer name: ");
+	
+		String s1 = scan.nextLine();
+		System.out.print("Enter youer name: ");
+		String s2 = scan.nextLine();
+		System.out.print("Enter youer name: ");
+		String s3 =scan.nextLine();
+		
+		System.out.println("Hi " + s0 +" "+ s1+" " +s2+" "+s3+" a");
 		//
 		// Say hello to the names in s0 through s3.
 		//
